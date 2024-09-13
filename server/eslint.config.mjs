96c5c629e7d7034b89c82d8b76768de52f7b734b
borislavpachev <borislav.pachev@gmail.com>
2024-09-13 19:53:36 +0300

    ES Lint configuration

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,9 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        process: true,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
