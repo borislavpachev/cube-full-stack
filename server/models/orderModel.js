f7beacb96f131205e23f95a6de242e230088b834
borislavpachev <borislav.pachev@gmail.com>
2024-09-16 11:45:35 +0300

    .

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const orderSchema = new Schema({});
+const orderSchema = new Schema({
+  userId: {
+    type: String,
+    required: [true, 'An order must have a user'],
+  },
+  products: {
+    type: Array,
+    default: [],
+  },
+  totalPrice: {
+    type: Number,
+  },
+  createdAt: Date,
+  status: {
+    type: String,
+    enum: ['In Progress', 'Shipped'],
+  },
+});
 
 const Order = model('Order', orderSchema);
 
