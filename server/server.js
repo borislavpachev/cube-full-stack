c310994c80dd80b4311acd836ade8601f47db8d1
borislavpachev <borislav.pachev@gmail.com>
2024-09-17 19:50:59 +0300

    unhandled rejections handler

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const app = require('./app');
+const connectDB = require('./config/db');
 
+connectDB();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is listening on port ${port}..`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
 });
