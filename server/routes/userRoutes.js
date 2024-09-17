ab059ac67dee98c344f34cbca2d1e0d074794357
borislavpachev <borislav.pachev@gmail.com>
2024-09-17 19:54:11 +0300

    current user profile update with protected and restricted middlewares

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,17 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.use(authController.protect);
+
+router.patch('/me/update-password', userController.updateCurrentUserPassword);
+router
+  .route('/me')
+  .patch(userController.updateMe)
+  .delete(userController.deleteMe);
+
+router.use(authController.restrictTo('Admin'));
 
 router
   .route('/')
