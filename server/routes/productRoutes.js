2810851e6af424e5dca5603228d74ced34f016e7
borislavpachev <borislav.pachev@gmail.com>
2024-09-17 19:54:44 +0300

    protected and restricted middlewares added

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const authController = require('../controllers/authController');
+
+router.use(authController.protect);
 
 router
   .route('/')
   .get(productController.getAllProducts)
-  .post(productController.createProduct);
+  .post(authController.restrictTo('Admin'), productController.createProduct);
 
 router
   .route('/:id')
   .get(productController.getProduct)
-  .patch(productController.updateProduct)
-  .delete(productController.deleteProduct);
+  .patch(authController.restrictTo('Admin'), productController.updateProduct)
+  .delete(authController.restrictTo('Admin'), productController.deleteProduct);
 
 module.exports = router;
