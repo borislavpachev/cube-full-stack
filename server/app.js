b79099d832f97473832e1e4345c6cc8a61af06dd
borislavpachev <borislav.pachev@gmail.com>
2024-09-22 14:00:32 +0300

    feat: A new cart router added

--- server/app.js
+++ server/app.js
@@ -6,14 +6,16 @@ const middlewares = require('./middleware/index');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const cartRouter = require('./routes/cartRoutes');
 
 const app = express();
 middlewares(app);
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/cart', cartRouter);
 
 app.all('*', (req, res, next) => {
   const error = new AppError(
