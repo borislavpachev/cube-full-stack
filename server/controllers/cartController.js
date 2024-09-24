57a916abd4144986b6ef3c3be35ce455ca9e5298
borislavpachev <borislav.pachev@gmail.com>
2024-09-24 17:21:46 +0300

    refactor: Add and remove items to the shopping cart, business logic moved outside controllers

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,18 +1,17 @@
-const User = require('../models/userModel');
-const Product = require('../models/productModel');
 const CustomError = require('../utils/CustomError');
 const httpStatus = require('../utils/httpStatus');
+const cartService = require('../services/cartService');
 
 exports.getCart = async (req, res, next) => {
   try {
-    const userId = req.user._id;
-    const user = await User.findById(userId);
-    const userCart = user.shoppingCart;
+    const shoppingCart = await cartService.getCart(req);
+
+    if (!shoppingCart) return;
 
     res.status(httpStatus.OK).json({
       status: 'success',
       data: {
-        shoppingCart: userCart,
+        shoppingCart,
       },
     });
   } catch (error) {
@@ -22,78 +21,9 @@ exports.getCart = async (req, res, next) => {
 
 exports.addToCart = async (req, res, next) => {
   try {
-    const {
-      _id: productId,
-      quantity: productQuantity,
-      size: productSize,
-    } = req.body;
+    const updatedUser = await cartService.addToCart(req, res, next);
 
-    const userId = req.user._id;
-    const user = await User.findById(userId);
-
-    let shoppingCartItem;
-    let shoppingCart = [...user.shoppingCart];
-
-    const product = await Product.findById(productId);
-
-    if (!product) {
-      return next(
-        new CustomError('This product does not exist', httpStatus.NOT_FOUND)
-      );
-    } else if (product.quantity[productSize] < productQuantity) {
-      return next(
-        new CustomError(
-          'Not enough products left in stock',
-          httpStatus.BAD_REQUEST
-        )
-      );
-    } else {
-      shoppingCartItem = {
-        productId,
-        quantity: productQuantity,
-        size: productSize,
-      };
-    }
-
-    const final = shoppingCart.reduce((acc, item) => {
-      if (
-        item.productId === shoppingCartItem.productId &&
-        item.size === productSize
-      ) {
-        item.quantity += productQuantity;
-        acc.push(item);
-      } else {
-        acc.push(item);
-      }
-      return acc;
-    }, []);
-
-    const productExists = shoppingCart.some(
-      (item) =>
-        item.productId === shoppingCartItem.productId &&
-        item.size === productSize
-    );
-
-    if (!productExists) {
-      final.push(shoppingCartItem);
-    }
-
-    product.quantity[productSize] - productQuantity;
-    const updatedQuantity = product.quantity;
-    console.log(updatedQuantity);
-    // await Product.findByIdAndUpdate(productId,
-    //     {quantity: },
-    //   {  runValidators:true,}
-    // )
-
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      { shoppingCart: final },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+    if (!updatedUser) return;
 
     res.status(httpStatus.OK).json({
       status: 'success',
@@ -106,9 +36,19 @@ exports.addToCart = async (req, res, next) => {
   }
 };
 
-// exports.emptyCart = async (req, res, next) => {
-//   try {
-//   } catch (error) {
-//     return next(new CustomError(error.message, error.status));
-//   }
-// };
+exports.removeItemFromCart = async (req, res, next) => {
+  try {
+    const updatedUser = await cartService.removeFromCart(req, res, next);
+
+    if (!updatedUser) return;
+
+    res.status(httpStatus.OK).json({
+      status: 'success',
+      data: {
+        shoppingCart: updatedUser.shoppingCart,
+      },
+    });
+  } catch (error) {
+    return next(new CustomError(error.message, error.status));
+  }
+};
