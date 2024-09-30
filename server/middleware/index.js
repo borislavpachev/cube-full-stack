14181dd6c8619ec14db79a28861f8255982c7658
borislavpachev <borislav.pachev@gmail.com>
2024-09-30 19:20:16 +0300

    chore: Cookie parser used

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,14 +3,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const cookieParser = require('cookie-parser');
 
 const rateLimit = require('./rateLimiter');
 
 module.exports = (app) => {
   app.use(helmet());
-  app.use(cors());
+  app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
   app.use(morgan('dev'));
   app.use(express.json());
   app.use(mongoSanitize());
-  app.use('/api', rateLimit);
+  app.use(cookieParser());
+   app.use('/api', rateLimit);
 };
