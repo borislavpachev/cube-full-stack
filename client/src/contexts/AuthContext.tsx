642909d11afb48d913a9388fa546072b5e33a425
borislavpachev <borislav.pachev@gmail.com>
2024-10-03 15:07:15 +0300

    feat: Authentication Context added to keep track on the logged in user

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,15 +1,51 @@
-import { createContext, type ReactNode, useState } from 'react';
+import { authenticate } from '@/services/authService';
+import { createContext, type ReactNode, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
-const AuthContext = createContext({ user: null });
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: 'Admin' | 'User';
+  phoneNumber: string;
+  favorites: [];
+  shoppingCart: [];
+  password: string;
+};
+
+type AuthContextValue = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+const initialValue = {
+  user: null,
+  setUser: () => {},
+};
+
+export const AuthContext = createContext<AuthContextValue | null>(initialValue);
 
 export default function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(initialValue.user);
+
+  useEffect(() => {
+    authenticate()
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
