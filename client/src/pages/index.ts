cc569b6953c189ff7708109ca1c0d2c956eb053b
borislavpachev <borislav.pachev@gmail.com>
2024-10-04 19:10:43 +0300

    refactor: Favorites page added

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from './Home/Home';
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
 export { default as Login } from './Login/Login';
 export { default as SignUp } from './SignUp/SignUp';
+export { default as Favorites } from './Favorites/Favorites';
