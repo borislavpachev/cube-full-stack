bc2f376ff6dc8266876c2e68401fa5d0749cd79d
borislavpachev <borislav.pachev@gmail.com>
2024-10-05 13:48:10 +0300

    feat: Section component used

---
+++ client/src/components/layout/Section.tsx
@@ -0,0 +1,16 @@
+import { type ReactNode } from 'react';
+
+type SectionProps = {
+  children: ReactNode;
+};
+
+export default function Section({ children }: SectionProps) {
+  return (
+    <section
+      className="max-w-3xl bg-white rounded flex flex-col 
+            items-center justify-center shadow-xl p-14"
+    >
+      {children}
+    </section>
+  );
+}
