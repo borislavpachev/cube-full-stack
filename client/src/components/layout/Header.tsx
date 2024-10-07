30e69ef0d4c673b5fc51ba4362539c2714bf86cd
borislavpachev <borislav.pachev@gmail.com>
2024-10-07 18:30:43 +0300

    fix: Conditionally render the user profile view and Login

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,19 +1,24 @@
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../constants';
 import { Input } from '../form';
+import { useContext } from 'react';
+import { AuthContext } from '@/contexts/AuthContext';
+import { AuthContextType } from '@/contexts/types';
+import { Button } from '../buttons';
 
 export default function Header() {
+  const { isAuthenticated } = useContext(AuthContext) as AuthContextType;
   const navigate = useNavigate();
-  
+
   return (
     <header className="flex h-16 bg-secondary items-center justify-around">
       <div className="w-1/4 flex items-center justify-center">
-        <h1
+        <h1 className='font-gope'
           onClick={() => {
             navigate('/');
           }}
         >
-          LOGO
+          CUBE
         </h1>
       </div>
       <div className="flex space-x-7 w-1/4 items-center justify-center">
@@ -45,14 +50,22 @@ export default function Header() {
           </NavLink>
         </li>
         <li>
-          <NavLink to={ROUTES.USER_PROFILE}>
-            <img
-              src="/images/user-profile.svg"
-              alt="User profile"
-              width={35}
-              height={35}
-            />
-          </NavLink>
+          {isAuthenticated ? (
+            <NavLink to={ROUTES.USER_PROFILE}>
+              <img
+                src="/images/user-profile.svg"
+                alt="User profile"
+                width={35}
+                height={35}
+              />
+            </NavLink>
+          ) : (
+            <div className="flex items-center">
+              <Button disabled={false} onClick={() => navigate('/login')}>
+                Login
+              </Button>
+            </div>
+          )}
         </li>
       </ul>
     </header>
