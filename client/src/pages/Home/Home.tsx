11c3454af98d1254840be2c322a403353c668c3c
borislavpachev <borislav.pachev@gmail.com>
2024-10-09 13:30:46 +0300

    fix: Naming conventions fix

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
-import { Product } from '@/components';
 import { MainLayout } from '@/components/layout';
+import { ProductComponent } from '@/components/productComponents';
 
 export default function Home() {
   return (
     <>
       <MainLayout>
         <div className="h-screen my-10">
-          <Product />
+          <ProductComponent />
         </div>
       </MainLayout>
     </>
