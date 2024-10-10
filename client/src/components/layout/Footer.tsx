7259d1f068a2223c07dd9e0a0123c24c66c45758
borislavpachev <borislav.pachev@gmail.com>
2024-10-10 21:01:58 +0300

    style: Better UI in footer component

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,7 +1,30 @@
-export default function Footer () {
-    return (
-        <footer className="text-center">
-            This is footer
-        </footer>
-    )
+import { Link } from 'react-router-dom';
+import Logo from '../Logo';
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-black p-5 text-white">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+        <Logo />
+        <div className="text-lg">
+          <Link to="mailto:cube@cube.com" className="hover:underline">
+            Contact Us
+          </Link>
+        </div>
+        <nav className="flex flex-col items-start justify-center">
+          <Link className="hover:underline" to="/">
+            Terms of Service
+          </Link>
+          <Link className="hover:underline" to="/">
+            Privacy policy
+          </Link>
+        </nav>
+      </div>
+      <div className="mt-6 text-center border-t border-white w-full">
+        <p className="mt-2">{`Copyright © ${year} - All right reserved by CUBE`}</p>
+      </div>
+    </footer>
+  );
 }
