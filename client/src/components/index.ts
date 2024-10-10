57f7dbdd2d15c5510c9c2ba391a6025953b4fca2
borislavpachev <borislav.pachev@gmail.com>
2024-10-10 20:47:37 +0300

    feat: Search component added

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,7 @@ export { default as CustomCarousel } from './CustomCarousel';
 export { default as Loading } from './Loading';
 export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as Modal } from './Modal';
+export { default as Logo } from './Logo';
+export { default as SearchComponent } from './SearchComponent';
+
+
