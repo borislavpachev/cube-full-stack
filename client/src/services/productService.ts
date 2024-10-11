8b7052f36886fc93f6daa67e2573b75eb5afa0f0
borislavpachev <borislav.pachev@gmail.com>
2024-10-11 14:39:05 +0300

    feat: getAllProducts from DB

--- client/src/services/productService.ts
+++ client/src/services/productService.ts
@@ -25,3 +25,29 @@ export const getProduct = async (id: string) => {
     return { error: 'An unexpected error occurred. Please try again!' };
   }
 };
+
+export const getAllProducts = async () => {
+  const url = `${API_BASE_URL}products`;
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return {
+        error: error.message || `Error getting All products`,
+      };
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return { error: 'An unexpected error occurred. Please try again!' };
+  }
+};
