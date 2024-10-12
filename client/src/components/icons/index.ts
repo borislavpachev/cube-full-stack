5b23e16858a2c7859143e4345bc5c907a17e6d73
borislavpachev <borislav.pachev@gmail.com>
2024-10-12 20:38:30 +0300

    style: New Icons added

---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,11 @@
+export { default as HeartIcon } from './HeartIcon';
+export { default as OpenEyeIcon } from './OpenEyeIcon';
+export { default as ClosedEyeIcon } from './ClosedEyeIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as ShoppingCartIcon } from './ShoppingCartIcon';
+export { default as UserIcon } from './UserIcon';
+export { default as InfoIcon } from './InfoIcon';
+export { default as PackageIcon } from './PackageIcon';
+export { default as LocationIcon } from './LocationIcon';
+export { default as LockIcon } from './LockIcon';
+
