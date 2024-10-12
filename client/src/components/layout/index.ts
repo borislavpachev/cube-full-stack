9c3140e43ba023e9ddefa1061e3d2fe961b3a249
borislavpachev <borislav.pachev@gmail.com>
2024-10-12 21:12:12 +0300

    refactor: Unnecessary components removed

--- client/src/components/layout/index.ts
+++ client/src/components/layout/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Section } from './Section';
-export { default as SectionTitle } from './SectionTitle';
 export { default as MainLayout } from './MainLayout';
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,6 +3,5 @@ export { default as Login } from './Login/Login';
 export { default as SignUp } from './SignUp/SignUp';
 export { default as Profile } from './Profile/Profile';
 export { default as Products } from './Products/Products';
-export { default as Favorites } from './Favorites/Favorites';
 export { default as ShoppingCart } from './ShoppingCart/ShoppingCart';
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
