9bd1c3880f6b33810429cd5d168f7b63c492c032
borislavpachev <borislav.pachev@gmail.com>
2024-10-12 21:15:18 +0300

    refactor: SideNav Component added and new Figma design layout in use

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,28 +1,15 @@
-import { Button } from '@/components/buttons';
 import { MainLayout } from '@/components/layout';
-import { ROUTES } from '@/constants';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { SideNav } from './components';
 
 export default function Profile() {
-  const navigate = useNavigate();
-
   return (
     <MainLayout>
-      <div className="flex flex-col md:w-3/4 items-center justify-start m-5">
-        <div className="max-w-5xl flex w-full items-center justify-center space-x-2 bg-white rounded p-2">
-          <Button onClick={() => navigate(`${ROUTES.USER_PROFILE}/orders`)}>
-            Orders
-          </Button>
-          <Button onClick={() => navigate(`${ROUTES.USER_PROFILE}/details`)}>
-            Profile
-          </Button>
-          <Button onClick={() => {}}>Address</Button>
-          <Button onClick={() => navigate(`${ROUTES.USER_PROFILE}/security`)}>
-            Security
-          </Button>
-        </div>
-        <div className="max-w-5xl w-full flex flex-col items-center justify-center mt-5">
-          <Outlet />
+      <div className="w-full flex flex-col items-start justify-start px-20 py-10">
+        <p className="text-3xl">My Profile</p>
+        <div className="w-full flex flex-col md:flex-row mt-10">
+          <SideNav />
+          <Outlet/>
         </div>
       </div>
     </MainLayout>
