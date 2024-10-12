50f986a44fc70c5bfbf6eeb1e7eeb8c824752999
borislavpachev <borislav.pachev@gmail.com>
2024-10-12 21:13:12 +0300

    feat: Profile SideNav and SideNavItem added

--- client/src/pages/Profile/components/index.ts
+++ client/src/pages/Profile/components/index.ts
@@ -1,4 +1,7 @@
 export { default as Orders } from './Orders';
+export { default as Favorites } from './Favorites';
 export { default as ProfileDetails } from './ProfileDetails';
 export { default as Security } from './Security';
 export { default as DeleteMe } from './DeleteMe';
+export { default as SideNav } from './SideNav';
+export { default as SideNavItem } from './SideNavItem';
