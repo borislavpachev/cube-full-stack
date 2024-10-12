e64bd08b5c7332f4c74c00301ff1fe3482780ef4
borislavpachev <borislav.pachev@gmail.com>
2024-10-12 15:09:00 +0300

    fix: middleware order fix for using product without being logged in

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const authController = require('../controllers/authController');
 
-router.use(authController.protect);
+router.route('/').get(productController.getAllProducts);
 
+router.route('/:id').get(productController.getProduct);
+
+router.use(authController.protect);
 router
   .route('/')
-  .get(productController.getAllProducts)
   .post(authController.restrictTo('Admin'), productController.createProduct);
 
 router
   .route('/:id')
-  .get(productController.getProduct)
   .patch(authController.restrictTo('Admin'), productController.updateProduct)
   .delete(authController.restrictTo('Admin'), productController.deleteProduct);
 
