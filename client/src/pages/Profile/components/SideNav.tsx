94febb9e6b19daac599e0d8ad425d385c569263d
borislavpachev <borislav.pachev@gmail.com>
2024-10-13 19:59:36 +0300

    style: in case of undefined context set text to "User"

--- client/src/pages/Profile/components/SideNav.tsx
+++ client/src/pages/Profile/components/SideNav.tsx
@@ -18,7 +18,7 @@ export default function SideNav() {
     <div className="flex flex-col min-w-sm">
       <div className="border-2 border-b-0 p-3 space-y-1">
         <p>Hello 👋</p>
-        <p className="font-semibold">{`${user?.firstName} ${user?.lastName}`}</p>
+        <p className="font-semibold">{`${user?.firstName} ${user?.lastName}` || 'User'}</p>
       </div>
       <nav className="flex flex-col border-2 py-5">
         <SideNavItem
