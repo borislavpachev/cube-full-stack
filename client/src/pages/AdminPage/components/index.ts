ca67db03b7ca2ac17a5af8672d8651ceb3e4ee5c
borislavpachev <borislav.pachev@gmail.com>
2024-10-16 18:13:56 +0300

    feat: Admin users' specific components added

---
+++ client/src/pages/AdminPage/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Users } from './Users';
+export { default as CreateUser } from './CreateUser';
+export { default as SimpleNav } from './SimpleNav';
+export { default as DataTable } from './DataTable';
