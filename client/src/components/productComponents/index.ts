853a87a98015e7eff8dbf04e6f16edf5bd82a389
borislavpachev <borislav.pachev@gmail.com>
2024-10-17 11:39:36 +0300

    refactor: CategoryCard moved to product page components folder

--- client/src/components/productComponents/index.ts
+++ client/src/components/productComponents/index.ts
@@ -1,4 +1,3 @@
 export { default as ProductComponent } from './ProductComponent';
 export { default as ProductCard } from './ProductCard';
 export { default as ProductElement } from './ProductElement';
-export { default as CategoryCard } from './CategoryCard';
