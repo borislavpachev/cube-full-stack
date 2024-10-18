98477b077159ca11370edb0fc2ee2c97d8a1d6a9
borislavpachev <borislav.pachev@gmail.com>
2024-10-18 15:22:45 +0300

    refactor: Categories moved out of products page and single product page added

--- client/src/pages/ProductsPage/ProductsPage.tsx
+++ client/src/pages/ProductsPage/ProductsPage.tsx
@@ -1,17 +1,85 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ROUTES } from '@/constants';
+import toast from 'react-hot-toast';
+import { Button } from '@/components/buttons';
 import { MainLayout, Section } from '@/components/layout';
-import { productCategories } from '@/constants';
-import { CategoryCard } from './components';
+import { ProductCard } from '@/components/productComponents';
+import { ProductValue } from '@/components/productComponents/types';
+import { getProductsByGenderAndCategory } from '@/services/productService';
+import { capitalizeFirstLetter } from '@/utils/helpers';
+import { PointerRightIcon } from '@/components/icons';
 
 export default function ProductsPage() {
+  const [products, setProducts] = useState<ProductValue[] | undefined>([]);
+
+  const { gender, category } = useParams();
+  const navigate = useNavigate();
+
+  let capitalizedGender: string = '';
+  let capitalizedCategory: string = '';
+  if (gender && category) {
+    capitalizedGender = capitalizeFirstLetter(gender);
+    capitalizedCategory = capitalizeFirstLetter(category);
+  }
+
+  useEffect(() => {
+    getProductsByGenderAndCategory(capitalizedGender, capitalizedCategory)
+      .then((res) => {
+        if (res.error) {
+          toast.error(res.error);
+          return;
+        }
+        const result = res.data.products;
+
+        setProducts(result);
+      })
+      .catch((error) => {
+        setProducts([]);
+        toast.error(error);
+      });
+  }, [capitalizedGender, capitalizedCategory]);
+
   return (
     <MainLayout>
-      <Section>
-        <div className="w-6xl m-auto flex flex-wrap items-center justify-center p-10 gap-14">
-          {productCategories.map((category, index) => {
-            return <CategoryCard key={index} category={category} />;
-          })}
-        </div>
-      </Section>
+      {!products?.length ? (
+        <Section>
+          <p className="text-center text-xl flex items-center">
+            No products in this category.
+          </p>
+          <p className="text-center text-sm text-gray-500 mt-2">
+            Check our products in the other categories.
+          </p>
+          <div className="mt-12">
+            <Button
+              onClick={() => {
+                navigate(`${ROUTES.PRODUCTS}/${gender}`);
+              }}
+            >
+              Continue shopping
+            </Button>
+          </div>
+        </Section>
+      ) : (
+        <Section>
+          <div className="mb-20 space-y-12">
+            {capitalizedGender && capitalizedCategory && (
+              <p className="md:mt-5 w-full tracking-wider cursor-default flex items-end text-xl md:text-3xl underline underline-offset-4">
+                <span>{`${capitalizedGender}'s Wear`}</span>
+                <span>
+                  <PointerRightIcon size={30} />
+                </span>
+                <span>{`${capitalizedCategory}`}</span>
+              </p>
+            )}
+            <div className=" flex flex-wrap w-full gap-14 justify-center items-center">
+              {products.map((item) => {
+                return <ProductCard id={item._id} key={item._id} />;
+              })}
+            </div>
+          </div>
+        </Section>
+      )}
     </MainLayout>
   );
 }
---
+++ client/src/pages/SingleProductPage/SingleProductPage.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from '@/components/layout';
+import { ProductComponent } from '@/components/productComponents';
+
+export default function SingleProductPage() {
+  return (
+    <MainLayout>
+      <ProductComponent />
+    </MainLayout>
+  );
+}
