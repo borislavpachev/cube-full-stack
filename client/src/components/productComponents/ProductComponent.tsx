c80f24f19f364c4eda8ce6eb21f0af70b13bd854
borislavpachev <borislav.pachev@gmail.com>
2024-10-20 23:47:50 +0300

    refactor: Custom Counter in use, select sizes moved to a constant

--- client/src/components/productComponents/ProductComponent.tsx
+++ client/src/components/productComponents/ProductComponent.tsx
@@ -1,15 +1,16 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { Button } from '../buttons';
-import CustomCarousel from '../CustomCarousel';
-import { MainLayout, Section } from '../layout';
 import { useEffect, useState } from 'react';
-import { getProduct } from '@/services/productService';
+import { useNavigate, useParams } from 'react-router-dom';
+import CustomCarousel from '../CustomCarousel';
+import { Button } from '../buttons';
 import { HeartIcon } from '../icons';
+import { Section } from '../layout';
 import Loading from '../Loading';
 import ProductElement from './ProductElement';
+import { getProduct } from '@/services/productService';
 import { ProductValue, Sizes } from './types';
-import { ROUTES } from '@/constants';
+import { productSizes, ROUTES } from '@/constants';
 import { priceFormatted } from '@/utils/helpers';
+import CustomCounter from '../CustomCounter';
 
 const images = [{ src: '/images/Front.png' }, { src: '/images/Back.png' }];
 
@@ -18,11 +19,12 @@ export default function ProductComponent() {
   const [loading, setLoading] = useState(true);
   const [selectedSize, setSelectedSize] = useState<Sizes>('M');
   const [quantity, setQuantity] = useState(1);
+
   const roundedPrice = priceFormatted(product?.price);
+  const productQuantity = product?.quantity[selectedSize];
+
   const { id } = useParams();
   const navigate = useNavigate();
-
-  const productQuantity = product?.quantity[selectedSize];
 
   useEffect(() => {
     if (!id) {
@@ -43,7 +45,7 @@ export default function ProductComponent() {
       });
   }, [id, navigate]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSize = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedSize(e.target.value as Sizes);
   };
 
@@ -52,106 +54,94 @@ export default function ProductComponent() {
   }
 
   return (
-    <MainLayout>
-      <Section>
-        <div className="flex flex-col space-y-10 md:space-y-0 md:space-x-10 md:flex-row items-start p-10 mx-20">
-          <CustomCarousel items={images} />
+    <Section>
+      <div className="flex flex-col space-y-10 md:space-y-0 md:space-x-10 md:flex-row items-start p-10 mx-20">
+        <CustomCarousel items={images} />
 
-          <div className="space-y-5 w-full">
-            <div className="flex flex-col md:flex-row items-start justify-between py-1">
-              <h1 className="text-4xl font-bold">{product?.name}</h1>
-              <HeartIcon size={40} />
-            </div>
+        <div className="space-y-4 w-full">
+          <div className="flex flex-col md:flex-row items-start justify-between py-1">
+            <h1 className="text-4xl font-bold">{product?.name}</h1>
+            <HeartIcon size={40} />
+          </div>
+          <div>
+            <ProductElement>{product?.gender}</ProductElement>
+            <p className="text-gray-500 line-clamp-1">{product?.description}</p>
+          </div>
+
+          <div className="flex flex-col justify-start space-y-3">
+            <p className="text-3xl font-bold">${roundedPrice}</p>
+
             <div>
-              <ProductElement>{product?.gender}</ProductElement>
-              <p className="text-gray-500 line-clamp-1">
-                {product?.description}
-              </p>
-            </div>
-
-            <div className="flex flex-col justify-start space-y-3">
-              <p className="text-3xl font-bold">${roundedPrice}</p>
-
-              <div className="space-y-1">
-                <ProductElement>Category</ProductElement>
-                <div className="w-1/2">
-                  <Button
-                    onClick={() =>
-                      navigate(
-                        `${ROUTES.PRODUCTS}/${product?.gender}/${product?.category}`
-                      )
-                    }
-                  >
-                    {product?.category}
-                  </Button>
-                </div>
-              </div>
-
-              <div>
-                <ProductElement>Color</ProductElement>
-                <div
-                  className={`bg-${product?.color} w-14 h-14 border-2 border-black rounded`}
-                  title={`${product?.color.toUpperCase()}`}
-                ></div>
-              </div>
-              <div>
-                <ProductElement>Size</ProductElement>
-
-                <select
-                  className="appearance-auto rounded outline-none py-4 text-xl w-14 h-14 border-2 border-black"
-                  name="sizes"
-                  id="select-sizes"
-                  value={selectedSize}
-                  onChange={handleChange}
+              <ProductElement>Category</ProductElement>
+              <div className="w-1/2">
+                <Button
+                  onClick={() =>
+                    navigate(
+                      `${ROUTES.PRODUCTS}/${product?.gender}/${product?.category}`
+                    )
+                  }
                 >
-                  <option value="XS">XS</option>
-                  <option value="S">S</option>
-                  <option value="M">M</option>
-                  <option value="L">L</option>
-                  <option value="XL">XL</option>
-                  <option value="XXL">XXL</option>
-                </select>
+                  {product?.category}
+                </Button>
               </div>
             </div>
 
             <div>
-              <p className="mb-1">
-                <span className="inline-block mr-1 rounded-full bg-green-400 w-3 h-3"></span>
-                <span className="font-semibold">
-                  {`${productQuantity} pcs left in stock`}
-                </span>
-              </p>
+              <ProductElement>Color</ProductElement>
+              <div
+                className={`bg-${product?.color} w-16 h-16 border-2 border-black rounded`}
+                title={`${product?.color}`}
+              ></div>
+            </div>
+            <div>
+              <ProductElement>Size</ProductElement>
 
-              <div className="flex space-x-5">
-                <div className="flex w-1/2 text-2xl items-center justify-around rounded border-2 border-black">
-                  <button
-                    className="p-2"
-                    onClick={() => setQuantity(quantity - 1)}
-                  >
-                    -
-                  </button>
-                  <div>{quantity}</div>
-                  <button
-                    className="p-2"
-                    onClick={() => setQuantity(quantity + 1)}
-                  >
-                    +
-                  </button>
-                </div>
-                <Button onClick={() => {}}>Add To Cart</Button>
-              </div>
+              <select
+                className="rounded outline-none text-xl w-16 h-16 border-2 border-black"
+                name="sizes"
+                id="select-sizes"
+                value={selectedSize}
+                onChange={handleSize}
+              >
+                {productSizes.map((size, index) => {
+                  return (
+                    <option key={index} value={size}>
+                      {size}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <p className="mb-1">
+              <span className="inline-block mr-1 rounded-full bg-green-400 w-3 h-3"></span>
+              <span className="font-semibold">
+                {`${productQuantity} pcs left in stock`}
+              </span>
+            </p>
+
+            <div className="flex space-x-5">
+              <CustomCounter
+                productQuantity={product?.quantity}
+                quantity={quantity}
+                size={selectedSize}
+                setQuantity={setQuantity}
+              />
+              <Button onClick={() => {}}>Add To Cart</Button>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col justify-start w-full px-20 mb-14">
-          <p className="text-2xl border-b-2 border-black w-fit">
-            Full Description
-          </p>
-          <div className="border-b-2"></div>
-          <p className="text-gray-500 text-lg my-3">{product?.description}</p>
-        </div>
-      </Section>
-    </MainLayout>
+      <div className="flex flex-col justify-start w-full px-20 mb-14">
+        <p className="text-2xl border-b-2 border-black w-fit">
+          Full Description
+        </p>
+        <div className="border-b-2"></div>
+        <p className="text-gray-500 text-lg my-3">{product?.description}</p>
+      </div>
+    </Section>
   );
 }
