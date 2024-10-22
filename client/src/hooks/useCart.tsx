3bd7d649a04963d1eb5f98d35b1328a8695f0bf5
borislavpachev <borislav.pachev@gmail.com>
2024-10-22 20:44:28 +0300

    feat: Remove shopping cart item working service in use

--- client/src/hooks/useCart.tsx
+++ client/src/hooks/useCart.tsx
@@ -1,5 +1,5 @@
 import { Sizes } from '@/components/product/types';
-import { addToShoppingCart } from '@/services';
+import { addToShoppingCart, removeFromShoppingCart } from '@/services';
 import toast from 'react-hot-toast';
 import useAuth from './useAuth';
 import { User } from '@/contexts/types';
@@ -31,5 +31,29 @@ export default function useCart(id: string, size: Sizes, quantity: number = 1) {
     }
   };
 
-  return { addToCart };
+  const removeFromCart = async () => {
+    try {
+      const productToRemove = {
+        _id: id,
+        quantity: quantity,
+        size: size,
+      };
+      const result = await removeFromShoppingCart(productToRemove);
+
+      if (result.error) {
+        toast.error(result.error);
+        return;
+      }
+
+      const updatedCart = result.data.shoppingCart;
+      setUser({ ...(user as User), shoppingCart: updatedCart });
+
+      toast.success('Product removed from shopping cart');
+    } catch (error) {
+      console.error(error);
+      toast.error('An unexpected error occurred. Please try again!');
+    }
+  };
+
+  return { addToCart, removeFromCart };
 }
