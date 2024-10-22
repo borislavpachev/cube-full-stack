1ec5935a9a00ba9dd15e4737701f26ddc90d26c0
borislavpachev <borislav.pachev@gmail.com>
2024-10-22 13:19:15 +0300

    fix: Service file following the same naming convention as the rest of the files

---
+++ client/src/services/index.ts
@@ -0,0 +1,7 @@
+export * from './authService';
+export * from './adminService';
+export * from './cartService';
+export * from './favoriteService';
+export * from './userService';
+export * from './productService';
+export * from './orderService';
