d46737e8816ed653969fe8f211318c597be6bb29
borislavpachev <borislav.pachev@gmail.com>
2024-10-22 21:49:30 +0300

    feat: Items appear in the shopping cart

--- client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
+++ client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
@@ -4,16 +4,22 @@ import { ShoppingCartType } from '@/contexts/types';
 import { getShoppingCart } from '@/services/cartService';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CartItem, CartNav, CartTotal } from './components';
+import toast from 'react-hot-toast';
 
 export default function ShoppingCartPage() {
   const [cart, setCart] = useState<ShoppingCartType[]>([]);
+  const [totalPrice, setTotalPrice] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
     getShoppingCart()
       .then((res) => {
-        console.log(res);
-        
+        if (res.error) {
+          toast.error(res.error);
+          return;
+        }
+
         const fetchedCart = res.data.shoppingCart;
         setCart(fetchedCart);
       })
@@ -23,11 +29,19 @@ export default function ShoppingCartPage() {
       });
   }, []);
 
+  const deleteCartItem = (id: string, size: string) => {
+    setCart((prevCart) => {
+      return prevCart?.filter((item) => {
+        return item._id !== id || (item._id === id && item.size !== size);
+      });
+    });
+  };
+
   return (
     <MainLayout>
-      <div className="w-full flex flex-col items-start justify-start px-20 py-10">
-        <p className="text-3xl">Shopping cart</p>
-        <div className="w-full mt-10 items-center justify-center">
+      <div className="w-full flex flex-col items-start justify-start p-0 md:p-10">
+        <p className="text-3xl px-10">Shopping cart</p>
+        <div className="w-full flex mt-5 items-center justify-center">
           {!cart.length ? (
             <Section>
               <p className="text-center text-xl flex items-center">
@@ -47,7 +61,27 @@ export default function ShoppingCartPage() {
               </div>
             </Section>
           ) : (
-            <Section>yes</Section>
+            <Section>
+              <div className="flex flex-col md:flex-row gap-5 w-full">
+                <div className="w-full">
+                  <CartNav />
+                  {cart.map((item, index) => {
+                    return (
+                      <CartItem
+                        id={item._id}
+                        quantity={item.quantity}
+                        size={item.size}
+                        key={index}
+                        deleteCartItem={deleteCartItem}
+                      />
+                    );
+                  })}
+                </div>
+                <div className="my-10 md:my-16 w-full md:w-1/3">
+                  <CartTotal totalPrice={totalPrice} />
+                </div>
+              </div>
+            </Section>
           )}
         </div>
       </div>
