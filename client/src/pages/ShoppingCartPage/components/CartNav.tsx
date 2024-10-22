d208e0588ecf3f463b77ab452888f0aab09e513e
borislavpachev <borislav.pachev@gmail.com>
2024-10-22 23:23:12 +0300

    style: Better UI of shopping cart items

--- client/src/pages/ShoppingCartPage/components/CartNav.tsx
+++ client/src/pages/ShoppingCartPage/components/CartNav.tsx
@@ -2,10 +2,10 @@ export default function CartNav() {
   return (
     <div className="w-full">
       <div className="hidden md:flex text-center border-b p-5 space-x-0 md:space-x-10 w-full">
-        <p className="w-2/5 text-start">Products</p>
-        <p className="w-1/5 text-start">Price</p>
-        <p className="w-1/5 text-start">Quantity</p>
-        <p className="w-1/5 text-start">Total</p>
+        <p className="w-2/5 text-start font-semibold">Products</p>
+        <p className="w-1/5 text-start font-semibold px-3">Price</p>
+        <p className="w-1/5 text-start font-semibold">Quantity</p>
+        <p className="w-1/5 text-start font-semibold">Total</p>
       </div>
       <div className="border-b md:hidden"></div>
     </div>
