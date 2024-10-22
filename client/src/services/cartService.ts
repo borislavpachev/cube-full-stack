75cb6a9603463c4b43b77625d2535a5ecce43dc1
borislavpachev <borislav.pachev@gmail.com>
2024-10-22 13:22:05 +0300

    feat: add and remove items from cart

--- client/src/services/cartService.ts
+++ client/src/services/cartService.ts
@@ -1,4 +1,11 @@
+import { Sizes } from '@/components/product/types';
 import { cartURL } from '@/constants';
+
+type Product = {
+  _id: string;
+  quantity?: number;
+  size: Sizes;
+};
 
 export const getShoppingCart = async () => {
   try {
@@ -19,6 +26,58 @@ export const getShoppingCart = async () => {
     }
 
     const data = await response.json();
+    return data;
+  } catch (error) {
+    return { error: 'An unexpected error occurred. Please try again!' };
+  }
+};
+
+export const addToShoppingCart = async (product: Product) => {
+  try {
+    const response = await fetch(cartURL, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return {
+        error:
+          error.message || 'Add product to cart failed! Please try again!',
+      };
+    }
+
+    const data = response.json();
+    return data;
+  } catch (error) {
+    return { error: 'An unexpected error occurred. Please try again!' };
+  }
+};
+
+export const removeFromShoppingCart = async (product: Product) => {
+  try {
+    const response = await fetch(cartURL, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return {
+        error:
+        error.message || 'Remove product from cart failed! Please try again!',
+      };
+    }
+
+    const data = response.json();
     return data;
   } catch (error) {
     return { error: 'An unexpected error occurred. Please try again!' };
