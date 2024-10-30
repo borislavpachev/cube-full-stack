eeb5509b5a1124591a712264b4924a78c24e33cf
borislavpachev <borislav.pachev@gmail.com>
2024-10-30 13:34:51 +0200

    feat: Update Product component in use

--- client/src/pages/AdminPage/components/index.ts
+++ client/src/pages/AdminPage/components/index.ts
@@ -5,3 +5,4 @@ export { default as SimpleNav } from './SimpleNav';
 export { default as DataTableUsers } from './DataTableUsers';
 export { default as CreateProduct } from './CreateProduct';
 export { default as DataTableProducts } from './DataTableProducts';
+export { default as UpdateProduct } from './UpdateProduct';
