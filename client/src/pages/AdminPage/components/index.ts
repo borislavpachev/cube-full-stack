304ce61b22c701f2bb97245433be9e3d57beefb3
borislavpachev <borislav.pachev@gmail.com>
2024-10-31 00:45:06 +0200

    refactor: Components moved out of main components folder in two separate users and products components

--- client/src/pages/AdminPage/components/index.ts
+++ client/src/pages/AdminPage/components/index.ts
@@ -1,9 +1,1 @@
-export { default as UsersPanel } from './UsersPanel';
-export { default as ProductPanel } from './ProductPanel';
-export { default as CreateUser } from './CreateUser';
 export { default as SimpleNav } from './SimpleNav';
-export { default as DataTableUsers } from './DataTableUsers';
-export { default as CreateProduct } from './CreateProduct';
-export { default as DataTableProducts } from './DataTableProducts';
-export { default as UpdateProduct } from './UpdateProduct';
-export { default as ProductActions } from './ProductActions';
