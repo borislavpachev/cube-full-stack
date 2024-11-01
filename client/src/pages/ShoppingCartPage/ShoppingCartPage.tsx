d3d92ff18f082d2e4e1586a2ff70d2e5d90f9182
borislavpachev <borislav.pachev@gmail.com>
2024-11-01 16:26:12 +0200

    feat: Shopping cart page is using the auth context for shopping cart and the item quantity fixed when removing or deleting items from cart

--- client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
+++ client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,57 +1,33 @@
 import { Button } from '@/components/buttons';
 import { MainLayout, Section } from '@/components/layout';
-import { ShoppingCartType } from '@/contexts/types';
-import { getShoppingCart } from '@/services/cartService';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CartItem, CartNav, CartTotal } from './components';
-import toast from 'react-hot-toast';
+import { useAuth } from '@/hooks';
+import { useEffect, useState } from 'react';
+import { ShoppingCartType } from '@/contexts/types';
 
 export default function ShoppingCartPage() {
-  const [cart, setCart] = useState<ShoppingCartType[]>([]);
+  const { user } = useAuth();
   const [totalPrice, setTotalPrice] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
-    getShoppingCart()
-      .then((res) => {
-        if (res.error) {
-          toast.error(res.error);
-          return;
-        }
-
-        const fetchedCart = res.data.shoppingCart;
-        setCart(fetchedCart);
-
-        const price = fetchedCart.reduce(
-          (total: number, item: ShoppingCartType) => {
-            total += item.quantity * item.price;
-            return total;
-          },
-          0
-        );
-        setTotalPrice(price);
-      })
-      .catch((error) => {
-        setCart([]);
-        console.log(error);
-      });
-  }, []);
-
-  const deleteCartItem = (id: string, size: string) => {
-    setCart((prevCart) => {
-      return prevCart?.filter((item) => {
-        return item._id !== id || (item._id === id && item.size !== size);
-      });
-    });
-  };
+    const price = user?.shoppingCart.reduce(
+      (total: number, item: ShoppingCartType) => {
+        total += item.quantity * item.price;
+        return total;
+      },
+      0
+    );
+    setTotalPrice(price as number);
+  }, [user?.shoppingCart]);
 
   return (
     <MainLayout>
       <div className="w-full flex flex-col items-start justify-start p-0 md:p-10">
         <p className="text-3xl px-10">Shopping cart</p>
         <div className="w-full flex mt-5 items-center justify-center">
-          {!cart.length ? (
+          {!user?.shoppingCart.length ? (
             <Section>
               <p className="text-center text-xl flex items-center">
                 You don't have items in your shopping cart yet.
@@ -74,15 +50,13 @@ export default function ShoppingCartPage() {
               <div className="flex flex-col md:flex-row gap-5 w-full">
                 <div className="w-full">
                   <CartNav />
-                  {cart.map((item, index) => {
+                  {user.shoppingCart.map((item) => {
                     return (
                       <CartItem
                         id={item._id}
                         quantity={item.quantity}
                         size={item.size}
-                        key={index}
-                        deleteCartItem={deleteCartItem}
-                        setTotal={setTotalPrice}
+                        key={`${item._id}-${item.size}`}
                       />
                     );
                   })}
