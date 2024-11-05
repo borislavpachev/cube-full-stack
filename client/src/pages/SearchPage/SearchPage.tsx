bdf5dfd977175dbb6e12492705d9fe4157495e29
borislavpachev <borislav.pachev@gmail.com>
2024-11-05 18:41:22 +0200

    fix: Better filtering of products based on passed search keywords and product gender or category

--- client/src/pages/SearchPage/SearchPage.tsx
+++ client/src/pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,17 @@
 import { NewArrivals, ProductCard } from '@/components';
-import { Input } from '@/components/form';
 import { MainLayout, Section } from '@/components/layout';
 import { ProductValue } from '@/components/product/types';
+import { useForm } from '@/hooks';
 import { getAllProducts } from '@/services';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { Filters } from './components';
 
 export default function SearchPage() {
   const [search, setSearch] = useState('');
+  const { form, setForm, updateForm } = useForm({
+    gender: '',
+    category: '',
+  });
   const [allProducts, setAllProducts] = useState<ProductValue[]>([]);
 
   useEffect(() => {
@@ -21,55 +26,73 @@ export default function SearchPage() {
       });
   }, []);
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
+  const filteredProducts = useMemo(() => {
+    return allProducts.filter((product) => {
+      const nameMatch = product.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const categoryMatch = form.category
+        ? product.category === form.category
+        : true;
+      const genderMatch = form.gender ? product.gender === form.gender : true;
+      return nameMatch && categoryMatch && genderMatch;
+    });
+  }, [allProducts, search, form.category, form.gender]);
 
-    if (search) {
-      setAllProducts((prevProduct) => {
-        return prevProduct.filter((product: ProductValue) => {
-          return product.name.includes(search);
-        });
-      });
-    }
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setSearch(searchTerm);
+  };
+
+  const resetFilters = () => {
+    setSearch('');
+    setForm({
+      gender: '',
+      category: '',
+    });
   };
 
   return (
     <MainLayout>
-      <div className="space-y-20 mb-16 flex flex-col justify-center w-full">
-        <div className="px-10 w-full">
-          <div className="flex px-10 w-full md:w-1/3 self-start mt-5">
-            <Input
-              id="app-search-bar"
-              name="app-search-bar"
-              value={search}
-              onChange={handleSearch}
-              placeholder="Search items"
-            />
-          </div>
-          {!search ? (
-            <Section>
-              <div
-                className="w-full flex flex-col mt-10 md:mt-0 
-              items-center justify-center px-10"
-              >
-                <p className="text-center text-xl flex items-center">
-                  No search results.
-                </p>
-                <p className="text-center text-sm text-gray-500 mt-2">
-                  Type and find the best deals for you.
-                </p>
-              </div>
-            </Section>
-          ) : (
-            <Section>
-              <p className="self-start text-3xl mb-5">{`Results for '${search}'`}</p>
-              <div className="flex flex-wrap w-full gap-14 justify-center">
-                {allProducts.map((item) => {
-                  return <ProductCard key={item._id} id={item._id} />;
-                })}
-              </div>
-            </Section>
-          )}
+      <div className="space-y-10 mb-16 flex flex-col justify-center w-full">
+        <div className="px-20 flex flex-col md:flex-row mt-5">
+          <Filters
+            search={search}
+            form={form}
+            handleSearch={handleSearch}
+            updateForm={updateForm}
+            reset={resetFilters}
+          />
+          <Section>
+            {filteredProducts.length ? (
+              <>
+                <p className="self-start text-3xl mb-5">{`${
+                  !search && !form.gender && !form.category
+                    ? `All products`
+                    : `Results for '${search}/${form.gender}/${form.category}'`
+                }`}</p>
+                <div className="flex flex-wrap w-full gap-10 justify-start">
+                  {filteredProducts.map((item) => {
+                    return <ProductCard key={item._id} id={item._id} />;
+                  })}
+                </div>
+              </>
+            ) : (
+              <Section>
+                <div
+                  className="w-full flex flex-col mt-10 md:mt-0 
+                  items-center justify-center px-10"
+                >
+                  <p className="text-center text-xl flex items-center">
+                    No search results.
+                  </p>
+                  <p className="text-center text-sm text-gray-500 mt-2">
+                    Set search criteria to find the best deals for you.
+                  </p>
+                </div>
+              </Section>
+            )}
+          </Section>
         </div>
         <NewArrivals />
       </div>
