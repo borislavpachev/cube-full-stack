2bd7392f998bc18379eff8165d7ade8ebc3773d8
borislavpachev <borislav.pachev@gmail.com>
2024-11-06 10:37:27 +0200

    feat: Filter products by price

--- client/src/pages/SearchPage/SearchPage.tsx
+++ client/src/pages/SearchPage/SearchPage.tsx
@@ -8,9 +8,11 @@ import { Filters } from './components';
 
 export default function SearchPage() {
   const [search, setSearch] = useState('');
+  const [maxPrice, setMaxPrice] = useState(0);
   const { form, setForm, updateForm } = useForm({
     gender: '',
     category: '',
+    price: 0,
   });
   const [allProducts, setAllProducts] = useState<ProductValue[]>([]);
 
@@ -20,6 +22,18 @@ export default function SearchPage() {
         const products = res.data.products;
 
         setAllProducts(products);
+
+        const maxProductPrice = products.reduce(
+          (acc: number, product: ProductValue) => {
+            if (product.price >= acc) {
+              acc = product.price;
+            }
+            return acc;
+          },
+          0
+        );
+
+        setMaxPrice(maxProductPrice);
       })
       .catch((error) => {
         console.log(error);
@@ -31,13 +45,17 @@ export default function SearchPage() {
       const nameMatch = product.name
         .toLowerCase()
         .includes(search.toLowerCase());
+
       const categoryMatch = form.category
         ? product.category === form.category
         : true;
+
       const genderMatch = form.gender ? product.gender === form.gender : true;
-      return nameMatch && categoryMatch && genderMatch;
+
+      const priceMatch = form.price ? product.price <= form.price : true;
+      return nameMatch && categoryMatch && genderMatch && priceMatch;
     });
-  }, [allProducts, search, form.category, form.gender]);
+  }, [allProducts, search, form.category, form.gender, form.price]);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = e.target.value.toLowerCase();
@@ -49,6 +67,7 @@ export default function SearchPage() {
     setForm({
       gender: '',
       category: '',
+      price: 0,
     });
   };
 
@@ -62,14 +81,15 @@ export default function SearchPage() {
             handleSearch={handleSearch}
             updateForm={updateForm}
             reset={resetFilters}
+            maxPrice={maxPrice}
           />
           <Section>
             {filteredProducts.length ? (
               <>
                 <p className="self-start text-3xl mb-5">{`${
-                  !search && !form.gender && !form.category
+                  !search && !form.gender && !form.category && !form.price
                     ? `All products`
-                    : `Results for '${search}/${form.gender}/${form.category}'`
+                    : `Results ${search ? `for '${search}'` : ''}`
                 }`}</p>
                 <div className="flex flex-wrap w-full gap-10 justify-center">
                   {filteredProducts.map((item) => {
