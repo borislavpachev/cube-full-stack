c0599d9fc5f875c017eb072907f6b8ab169ba8a2
borislavpachev <borislav.pachev@gmail.com>
2024-11-07 19:29:23 +0200

    feat: Create new order service function added

--- client/src/services/orderService.ts
+++ client/src/services/orderService.ts
@@ -24,3 +24,28 @@ export const getMyOrders = async () => {
     return { error: 'An unexpected error occurred. Please try again!' };
   }
 };
+
+export const createOrder = async () => {
+  try {
+    const response = await fetch(ordersURL, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return {
+        error: error.message || 'Failed to create an order! Please try again!',
+      };
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return { error: 'An unexpected error occurred. Please try again!' };
+  }
+};
