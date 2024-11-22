392d2b945611e16e04c465beaa82543b83afbeb9
borislavpachev <borislav.pachev@gmail.com>
2024-11-22 21:46:48 +0200

    style: Better UI with background color based on selected gender

--- client/src/pages/CategoryPage/CategoryPage.tsx
+++ client/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,12 +1,15 @@
 import { MainLayout, Section } from '@/components/layout';
 import { productCategories } from '@/constants';
 import { CategoryCard } from './components';
+import { useParams } from 'react-router-dom';
 
 export default function CategoryPage() {
+  const {gender} = useParams();
+
   return (
     <MainLayout>
       <Section>
-        <div className="w-6xl m-auto flex flex-wrap items-center justify-center p-10 gap-14 mb-10">
+        <div className={`w-6xl ${gender === 'women' ? 'bg-red-100/50' : 'bg-blue-100/50'} m-auto flex flex-wrap items-center justify-center p-10 gap-14 mb-10`}>
           {productCategories.map((category, index) => {
             return <CategoryCard key={index} category={category} />;
           })}
